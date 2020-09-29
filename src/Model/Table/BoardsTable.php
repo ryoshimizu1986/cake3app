25fc89b4e8089b2ac2cf6033b01e2acb05655734
Ryo Shimizu <shimizu@1st-link.co.jp>
2020-09-29 13:59:25 +0900

    コールバックメソッド

--- src/Model/Table/BoardsTable.php
+++ src/Model/Table/BoardsTable.php
@@ -2,7 +2,20 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Event\Event;
+use Cake\ORM\Query;
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
 
 class BoardsTable extends Table {
-    
+    public function beforeSave(Event $event, EntityInterface $entity, $options){
+        $n = $this->find('all', ['conditions'=>['name'=>$entity->name]])->count();
+
+        if ($n == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
